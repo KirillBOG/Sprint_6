d6c4687c0d7af47624e28e85f335d5e32db5d405
Kirill Bogdanov <Bogdanovs4@yandex.ru>
2024-12-28 22:35:53 +0300

    Sprint_6v1

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+package com.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
+
+    @Spy
+    private Feline feline = new Feline();
+
+    @Test
+    public void getFamilyTest() {
+        Mockito.when(feline.getFamily()).thenReturn("Кошачьи");
+        Assert.assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensWithoutParams() {
+        feline.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens(1);
+    }
+
+    @Test
+    public void getKittensWithParamsInt() {
+        feline.getKittens(0);
+        Mockito.verify(feline).getKittens(Mockito.anyInt());
+    }
+    @Test
+    public void eatMeatTest() throws Exception {
+
+        List<String> actualFood = feline.eatMeat();
+        Mockito.verify(feline).getFood("Хищник");
+        Assert.assertNotNull(actualFood);
+    }
 }
